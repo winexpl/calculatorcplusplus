45b3c3e6d0bce56d4b89fce4246b975ae139fca1
masha <drevina2003@mail.ru>
2024-05-19 23:50:43 +0400

    ex1

--- src/main.cpp
+++ src/main.cpp
@@ -123,7 +123,7 @@ struct Tree {
 int main() {
     cout << "hi!" << endl;
 
-    double arr[]{1,2,NAN,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+    double arr[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
     Tree tree{arr};
     tree.output_tree(20);
 }
